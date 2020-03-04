411fa5ebbc270bf9b8b85dad853a771400db0e98
Ketherz <edmond.cuillier@gmail.com>
2020-03-04 17:12:06 +0100

    restart

--- kademlia/storage.py
+++ kademlia/storage.py
@@ -51,11 +51,17 @@ class ForgetfulStorage(IStorage):
         self.data = OrderedDict()
         self.ttl = ttl
 
+#============================Modifié par Edmond===================================#
+
     def __setitem__(self, key, value):
-        if key in self.data:
-            del self.data[key]
-        self.data[key] = (time.monotonic(), value)
+        if key not in self.data:
+            print("clef non présente")
+            self.data[key] = set()
+        print(self.data[key])
+        self.data[key].add((time.monotonic(), value))
         self.cull()
+        print(self.data[key])
+
 
     def cull(self):
         for _, _ in self.iter_older_than(self.ttl):

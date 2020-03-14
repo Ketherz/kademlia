4cd1a501ea862306d7e4fb17b07350b88269d4f3
Ketherz <edmond.cuillier@gmail.com>
2020-03-14 09:44:06 +0100

    nouvelle branche pour la modification du fichier network

--- kademlia/storage.py
+++ kademlia/storage.py
@@ -51,19 +51,11 @@ class ForgetfulStorage(IStorage):
         self.data = OrderedDict()
         self.ttl = ttl
 
-#============================Modifié par Edmond===================================#
-
     def __setitem__(self, key, value):
-        if key not in self.data:
-            print("clef non présente")
-            self.data[key] = (time.monotonic(), set())
-        else:
-            self.data[key] = (time.monotonic(), self.data[key][1])
-        print(self.data[key])
-        self.data[key][1].add(value)
+        if key in self.data:
+            del self.data[key]
+        self.data[key] = (time.monotonic(), value)
         self.cull()
-        print(self.data[key])
-
 
     def cull(self):
         for _, _ in self.iter_older_than(self.ttl):
